22311792ba81dcb0c99a3c6ba2560bbeaf247afc
Thomas Hansen <th4019@gmail.com>
2017-03-10 01:52:00 -0800

    getting facebook login/user creation working

--- app.js
+++ app.js
@@ -3,21 +3,60 @@ var app = express();
 var cors = require('cors');
 var bodyParser  = require('body-parser');
 
+var graph = require('fbgraph');
+var Promise = require("bluebird");
+Promise.promisifyAll(graph);
+var generator = require('generate-password');
+
 var expressa = require('expressa');
 
-expressa.addListener('post', 0, function(req, collection, doc) {
+function populateUserFromFacebook(doc) {
+  graph.setAccessToken(doc.fbAccessToken);
+  return graph.getAsync('/me?fields=email,first_name,last_name,gender,timezone,hometown,location,friends')
+    .then(function(res) {
+      if (res) {
+        doc.email = res.email;
+        doc.first_name = res.first_name;
+        doc.last_name = res.first_name;
+        doc.gender = res.gender;
+        doc.password = generator.generate({
+            length: 20,
+            numbers: true
+        });
+        doc.facebook_id = res.id;
+        delete doc.fbAccessToken;
+      }
+      return doc;
+    })
+}
+
+expressa.addListener('post', -10, function(req, collection, doc) {
   if (collection == 'users') {
     if (doc.transaction_id) {
-      return expressa.db.user_payments.find({'transaction_id': doc.transaction_id})
-        .then(function(results) {
-          if (results.length > 0) {
-            doc.roles = doc.roles || [];
-            doc.roles.push('ActivePass');
-            doc.passType = results[0]['sku'];
-            results[0].linked_user = doc.email;
-            expressa.db.user_payments.update(results[0]._id, results[0]);
-          }
-        });
+      var payment;
+      return new Promise(function(resolve, reject) {
+        expressa.db.user_payments.find({'transaction_id': doc.transaction_id})
+          .then(function(results) {
+            if (results.length > 0) {
+              payment = results[0];
+              doc.payment_id = payment._id;
+              doc.roles = doc.roles || [];
+              doc.roles.push('ActivePass');
+              doc.passType = results[0]['sku'];
+
+              if (doc.fbAccessToken) {
+                return populateUserFromFacebook(doc);
+              } else {
+                return doc;
+              }
+            }
+          })
+          .then(function(doc) {
+              payment.linked_user = doc.email;
+              expressa.db.user_payments.update(payment._id, payment);
+              resolve();
+          }, reject)
+      });
     }
   }
 })
