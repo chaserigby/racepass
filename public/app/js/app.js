d16f08fd9381ac6c214356c5037181aaa91fece0
root <root@ubuntu-1gb-sfo2-01.localdomain>
2017-04-04 09:23:18 +0000

    forcing logout with invalid token

--- public/app/js/app.js
+++ public/app/js/app.js
@@ -53,7 +53,15 @@ app.controller('MainController', function($location, $http, $scope, $timeout) {
             localStorage.lng = this.user.address.coordinates.lng;
             localStorage.city = this.user.address.city;
           }
-        }.bind(this));
+        }.bind(this),
+        function(err) {
+          if (err.status == 404) {
+            // user is not found, maybe old or invalid token
+            delete localStorage.uid;
+            delete localStorage.token;
+            window.location = '/';
+          }
+        });
     };
 
     this.update_user_info();
