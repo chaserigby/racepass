8f3fae01a5477b30989147c922c9ffa167c03e4e
David Hughes <hughes.davidallen@gmail.com>
2017-05-12 02:27:47 -0700

    First attempt at pagination for list search

--- public/app/js/list.js
+++ public/app/js/list.js
@@ -1,12 +1,36 @@
-angular.module('main').controller(
-    'ListController', function($timeout, $filter, $http, $location) {
-      this.raceInfo = [];
+angular.module('main').controller('ListController', function($timeout, $filter,
+                                                             $http, $location) {
+  this.raceInfo = [];
+  this.offset = 0;
+  this.limit = 10;
+  this.hasNextPage = true;
 
-      this.populateRaceInfo = function() {
-        var self = this;
-        $http.get(window.apiurl + 'race?limit=10&token=' + localStorage.token)
-            .then(function(result) { self.raceInfo = result.data; });
-      }.bind(this);
+  this.populateRaceInfo = function() {
+    var self = this;
+    $http
+        .get(window.apiurl + 'race?offset=' + this.offset +
+             'limit=' + (this.limit + 1) + '&token=' + localStorage.token)
+        .then(function(result) {
+          var data = result.data;
+          this.hasNextPage = data.length > this.limit;
+          self.raceInfo = result.data.slice(0, this.limit);
+        });
+  }.bind(this);
 
-      this.populateRaceInfo();
-    });
+  this.getNavMessage = function() {
+    return 'Results ' + (this.offset + 1) + ' through ' +
+           (this.offset + this.limit) + ' of many';
+  }.bind(this);
+
+  this.getPreviousPage = function() {
+    this.offset = Math.max(0, this.offset - this.limit);
+    this.populateRaceInfo();
+  }.bind(this);
+
+  this.getNextPage = function() {
+    this.offset += this.limit;
+    this.populateRaceInfo();
+  }.bind(this);
+
+  this.populateRaceInfo();
+});
